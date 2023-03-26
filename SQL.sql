3c74d287f6a33ad05717bbb4ac3f248e78b6ac2e
Ali <alidderrr@gmail.com>
2023-03-26 18:36:22 +0300

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,12 @@
+SORGU 1 = SELECT title , description FROM film
+
+SORGU 2 = SELECT * FROM film WHERE length > 60 AND length < 75 
+
+SORGU 3 = SELECT * FROM film 
+          WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99SORGU 1
+
+SORGU 4 = SELECT last_name FROM customer 
+          WHERE first_name = 'Mary'
+
+SORGU 5 = SELECT * FROM film
+          WHERE length <=50 And rental_rate != 2.99 OR rental_rate != 4.99    
